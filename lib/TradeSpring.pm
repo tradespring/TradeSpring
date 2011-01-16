602be3f6f30b5012060a973e3e8c9229bf7f60de
Chia-liang Kao <clkao@clkao.org>
2011-01-16 13:06:57 +0800

    remove rpt-based tick runner

--- lib/TradeSpring.pm
+++ lib/TradeSpring.pm
@@ -155,10 +155,7 @@ sub run_trade {
             run_tick_fitf($strategy, $lb, $dt, $time);
         }
         else {
-            my ($date, $time) = split(/ /, $strategy->calc->prices->at($i)->[$DATE]);
-            my $dt = $strategy->can('current_date') ?
-                $strategy->current_date : $Strp->parse_datetime($date);
-            run_tick_until($strategy, $lb, $dt, $time);
+            warn "not sure what to do";
         }
     }
 
@@ -257,72 +254,6 @@ sub run_tick_fitf {
 sub fitf_store {
     my $date = shift;
     return '/Users/clkao/work/trade/XTAF.TX/'.$date->year.'/XTAF.TX-'.$date->ymd.'.fitf';
-}
-
-my $current_date;
-my $continue;
-my $cv;
-my $source;
-my $buffer;
-sub run_tick_until {
-    my ($daytrade, $lb, $date, $time) = @_;
-
-    $logger->debug("run tick until: $time ".$daytrade->date);
-    if (!$current_date || $current_date ne $date->ymd) {
-        $buffer = [];
-        $current_date = $date->ymd;
-#        warn "==> new date: $current_date".$/;
-        use Finance::TW::TAIFEX;
-        my $taifex = Finance::TW::TAIFEX->new($date);
-
-        my $contract = $taifex->product('TX')->near_term($taifex->context_date);
-        use AnyEvent;
-
-        use GTL::Source::File;
-        my $rpt_f = Finance::GeniusTrader::Conf::get("GLT::taifex_rpt_dir")."/$current_date.rpt" or die " can't find $date.rpt";
-        $cv = AE::cv;
-        $source = GTL::Source::File->new(
-            tick_delay => 0,
-            second_delay => 0,
-            rpt => $rpt_f,
-            contract_name => 'TX', contract_month => $contract,
-            cb_done => $cv,
-        );
-        my $done = 0;
-        $continue = AE::cv;
-        my (undef, $first) = split(/ /,$daytrade->date($daytrade->i - 1));
-        $first =~ s/://g;
-        $first =~ s/^0//;
-        $source->start(
-            sub {
-                return if $done;
-                return unless $_[0]{time} >= $first;
-
-                push @$buffer, $_[0];
-            });
-        $cv->recv;
-    }
-
-    $time =~ s/://g;
-    $time =~ s/^0//;
-#    warn "=> pop buffer until $time ".(scalar @$buffer).' / '.$buffer->[0]{time};
-    Carp::confess unless $buffer->[0]{time};
-
-    my (undef, $first) = split(/ /,$daytrade->date($daytrade->i - 1));
-    $first =~ s/://g;
-    $first =~ s/^0//;
-
-    while (scalar @$buffer && $buffer->[0]{time} < $time) {
-        my $f = shift @$buffer;
-        my ($date, $time) = @{$f}{qw(date time)};
-
-        next unless $f->{time} >= $first;
-
-        $date =~ s/(\d\d\d\d)(\d\d)(\d\d)/$1-$2-$3/;
-        $time =~ s/(\d\d?)(\d\d)(\d\d)/$1:$2:$3/;
-        $time = '0'.$time unless substr($time, 0, 1) eq '1';
-        $lb->on_price($f->{price}, $f->{volume}, "$date $time");
-    }
 }
 
 use Finance::GeniusTrader::Calculator;
