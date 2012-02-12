c8d09b00c5b4179081f7469dbfbe40f13126e94b
Chia-liang Kao <clkao@clkao.org>
2012-02-13 00:09:56 +0800

    allow minimum r to be defined in marketmoney ps module

--- lib/TradeSpring/PS/MarketMoney.pm
+++ lib/TradeSpring/PS/MarketMoney.pm
@@ -20,6 +20,8 @@ has mm_risk => (is => "rw", isa => "Num", default => sub { 0.01 });
 
 has maxqty => (is => "rw", isa => "Int", default => sub { 9999 });
 
+has minr => (is => "rw", isa => "Int", default => sub { 0 });
+
 method info {
     sprintf 'MarketMoney(base risk: %5.2f%%, mm risk: %5.2f%%, max qty: %d): current equity: %d, base: %d',
         $self->base_risk * 100 , $self->mm_risk * 100, $self->maxqty, $self->equity, $self->base;
@@ -51,7 +53,7 @@ method _set_equity($e, $old) {
 method get_qty($r) {
     my $e = $self->equity;
     my $risk = ($e * $self->base_risk + max(0, ($e - $self->base)) * $self->mm_risk);
-    my $qty = int($risk / $r);
+    my $qty = int($risk / ($self->minr ? max($r, $self->minr) : $r));
 
     $self->log->info(sprintf('risking %.2f / %.2f, qty = %d / %d', $risk, $r, $qty, $self->maxqty))
         if $self->log->is_info;
