a0e0266b336a48dee70d83028675657a1f02b72b
Chia-liang Kao <clkao@clkao.org>
2012-04-21 12:03:22 +0800

    add FSA example

---
+++ examples/DBO.pm
@@ -0,0 +1,87 @@
+package DBO;
+use warnings;
+use 5.10.0;
+use Moose;
+use Try::Tiny;
+use methods;
+
+extends 'TradeSpring::Frame', 'TradeSpring::Strategy';
+with 'MooseX::Log::Log4perl';
+
+method manage_position {
+    sub {
+        my $state = shift;
+        my $stp_id = $state->notes('exit_id_map')->{'stp'};
+        my $stp = $self->broker->get_order($stp_id);
+
+        return unless $stp_id;
+        my $evx = 15;
+        my $ww = $self->ne_ww;
+        $ww->test($_) for ($self->i-$evx+1..$self->i);
+        my $price = $ww->current_value;
+        if ($self->bt($price, $stp->{order}{price})) {
+            $self->broker->update_order($stp_id, $price,
+                                        undef,
+                                        sub { $self->log->info('updated stp to '.$ww->current_value) });
+            $state->notes('stp_price', $price);
+        }
+        return 0;
+    }
+}
+
+method run {
+    $self->debug('run');
+    my $fsa = $self->fsa;
+    if (@$fsa) {
+        my @remaining;
+        for my $f (@$fsa) {
+            $self->debug('fsa at '.$f->curr_state->name);
+            if ($f->at('pending')) {
+                $self->broker->cancel_order( $f->notes('order'), sub {
+                                                 $self->debug("order @{[ $f->notes('order') ]} cancelled: ".join(',', @_) );
+                                 });
+
+            }
+            elsif ($f->at('closed')) {
+            } else {
+                $f->try_switch;
+                push @remaining, $f;
+            }
+        }
+        $fsa = \@remaining;
+        $self->fsa($fsa);
+
+    }
+    $self->debug('make orders? ');
+    if (@$fsa) {
+        return;
+    }
+
+    $self->debug('make orders ');
+    for my $dir (-1,1) {
+        $self->mk_order($dir);
+    }
+}
+
+method initial_stp { 0.02 }
+
+method mk_order($dir) {
+    local $self->{direction} = $dir;
+
+    my $evl =  22;
+    my $bb = $self->ne_bb;
+    $bb->test($_) for ($self->i-$evl+1..$self->i);
+
+    my $stp_price = $bb->current_value * ( 1 - $self->initial_stp * $dir);
+    my $qty = 1;
+
+    my $fsa = $self->new_fsa($dir, $bb->current_value, $qty, $stp_price);
+    $fsa->start;
+    push @{$self->fsa}, $fsa;
+}
+
+with 'TradeSpring::Directional', 'TradeSpring::FSA';
+
+__PACKAGE__->meta->make_immutable;
+
+1;
