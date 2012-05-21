4925487cc2b7da6a5b47e4b88f25c799f02905f2
Chia-liang Kao <clkao@clkao.org>
2012-05-22 06:55:17 +0800

    loose cross over

--- lib/TradeSpring/FrameVal/CrossOver.pm
+++ lib/TradeSpring/FrameVal/CrossOver.pm
@@ -14,15 +14,17 @@ has b => (
     isa => 'TradeSpring::FrameVal',
 );
 
+has loose => (is => "ro", isa => "Bool", default => sub { 0 });
+
 method do_get {
     my ($av, $bv) = ($->a, $->b);
     return unless defined $av && $bv;
     return unless defined $->a->[1] && $->b->[1];
-    if ($->a->[1] < $->b->[1] && $->a > $->b) {
-        return 1
+    if ($->a->[1] <= $->b->[1] && $->a > $->b) {
+        return $->a->[1] < $->b->[1] || $->loose ? 1 : 0;
     }
-    elsif ($->a->[1] > $->b->[1] && $->a < $->b) {
-        return -1;
+    elsif ($->a->[1] >= $->b->[1] && $->a < $->b) {
+        return $->a->[1] > $->b->[1] || $->loose ? -1 : 0;
     }
     return 0;
 }
